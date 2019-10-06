e1c093388b968d6ba287d1326d6a654fb3db92b3
nmarini <nmarini44@gmail.com>
2019-10-06 09:55:41 -0700

    set-up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const reducer = combineReducers({
+    
+})
+
+const composeEnhancer = window.__Redux_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 
