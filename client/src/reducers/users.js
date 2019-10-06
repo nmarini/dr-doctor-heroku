380e3723d57b8edc3519c955102ee5c28a4ea7f7
nmarini <nmarini44@gmail.com>
2019-10-06 15:21:59 -0700

    started creating users reducer

---
+++ client/src/reducers/users.js
@@ -0,0 +1,12 @@
+const usersReducer = (state = [], action) => {
+    switch (action.type) {
+        case '':
+            return (
+                Object.assign({}, state, {
+                    
+                })
+            )
+        default:
+            return state 
+    }
+}
