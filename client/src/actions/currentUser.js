c6ee4391905099161b735772302f6605d2b7de77
nmarini <nmarini44@gmail.com>
2019-10-06 15:52:14 -0700

    updated reducers

---
+++ client/src/actions/currentUser.js
@@ -0,0 +1,15 @@
+// synchronous action creators
+export const setCurrentUser = user => {
+    return {
+        type: "SET_CURRENT_USER",
+        user
+    }
+
+};
+
+// asynchronous action creators
+export const login = credentials => {
+    return dispatch => {
+        return fetch("http://localhost:3000")
+    }
+}
--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import diagnosesReducer from './reducers/diagnoses';
+import diagnosesReducer from './reducers/diagnoses.js';
+import currentUserReducer from './reducers/currentUserReducer.js'
 import thunk from 'redux-thunk';
 
 const reducer = combineReducers({
-    user: diagnosesReducer
+    diagnoses: diagnosesReducer,
+    currentUser: currentUserReducer
 })
 
 const composeEnhancer = window.__Redux_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
