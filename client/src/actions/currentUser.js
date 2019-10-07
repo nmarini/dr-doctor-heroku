9cbc2af6516f5a9abe15209e448039eb0145a053
nmarini <nmarini44@gmail.com>
2019-10-06 17:10:57 -0700

    updated currentUser action

--- client/src/actions/currentUser.js
+++ client/src/actions/currentUser.js
@@ -10,6 +10,12 @@ export const setCurrentUser = user => {
 // asynchronous action creators
 export const login = credentials => {
     return dispatch => {
-        return fetch("http://localhost:3000")
+        return fetch("http://localhost:3001/api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials) 
+        })
     }
 }
