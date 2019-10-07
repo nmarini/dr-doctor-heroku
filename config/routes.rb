2d41edf06bd9895258584f4643d31039005e07f0
nmarini <nmarini44@gmail.com>
2019-10-06 17:09:47 -0700

    added /login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  get "/api/v1/login", to: "sessions#create"
+
   namespace :api do 
     namespace :v1 do 
       resources :diagnoses, :only => [:show, :create, :update, :destroy]
       resources :users, :only => [:show, :create, :update, :destroy]
+      
     end 
   end 
   
