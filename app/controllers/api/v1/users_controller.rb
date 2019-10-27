908d7d7cc844aa58dd5660ad05d0f0a1e143f9d3
nmarini <nmarini44@gmail.com>
2019-10-26 18:23:38 -0700

    logged user in signup

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save 
+            session[:user_id] = @user.id 
             render json: @user, include: [:doctors]
         else 
             render json: @user.errors, status: :unprocessable_entity 
