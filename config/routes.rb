3f848308c765c6343af5dc905b2833e81f9024b2
nmarini <nmarini44@gmail.com>
2019-10-27 14:25:04 -0700

    deleted extra route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   post "/api/v1/login", to: "api/v1/sessions#create"
   post "/api/v1/signup", to: "api/v1/users#create"
-  post "/api/v1/create_doctor", to: "api/v1/doctors#create"
   delete "/api/v1/logout", to: "api/v1/sessions#destroy"
   get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
 
