32b3b18570834c9f54233d87e0674e7d1dfd377c
nmarini <nmarini44@gmail.com>
2019-10-29 14:09:26 -0700

    imported bootstrap css

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 require('dotenv').config();
-
+<link
+  rel="stylesheet"
+  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>
 ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 
  
