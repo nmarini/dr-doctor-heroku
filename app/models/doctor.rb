b80f2f298caafa6229a35fad0544af6ae6520fb1
nmarini <nmarini44@gmail.com>
2019-10-29 17:16:06 -0700

    validated uniqueness of doctor.uid within user_id scope

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,5 +1,5 @@
 class Doctor < ApplicationRecord
     belongs_to :user
     validates :last_name, :uid, :user_id, presence: true
-    validates :uid, uniqueness: true
+    validates :uid, uniqueness: { scope: :user_id}
 end
