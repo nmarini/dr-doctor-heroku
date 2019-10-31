a085695283e685934b28e83bebe22c855ac9db48
nmarini <nmarini44@gmail.com>
2019-10-31 16:06:47 -0700

    commented out component

--- client/src/containers/DoctorContainer.js
+++ client/src/containers/DoctorContainer.js
@@ -1,48 +1,48 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import DoctorList from '../components/DoctorList';
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import DoctorList from '../components/DoctorList';
 
-class DoctorContainer extends Component {
+// class DoctorContainer extends Component {
 
-    // constructor() {
-    //     super()
-    //     this.state = {
-    //         exposeList: false,
-    //         loaded: false
-    //     }
-    // }
+//     // constructor() {
+//     //     super()
+//     //     this.state = {
+//     //         exposeList: false,
+//     //         loaded: false
+//     //     }
+//     // }
 
-    // showList = (doctors) => <DoctorList doctors={doctors}/> 
+//     // showList = (doctors) => <DoctorList doctors={doctors}/> 
 
-    // handleClick = () => {
-    //     let newState = this.state.exposeList ? false : true
-    //     this.setState({
-    //         exposeList: newState
-    //     })
-    // }
+//     // handleClick = () => {
+//     //     let newState = this.state.exposeList ? false : true
+//     //     this.setState({
+//     //         exposeList: newState
+//     //     })
+//     // }
 
-    // componentDidMount() {
-    //     this.setState({
-    //         loaded: true
-    //     })
-    // }
+//     // componentDidMount() {
+//     //     this.setState({
+//     //         loaded: true
+//     //     })
+//     // }
 
 
 
-    render() {
-        return (
-            <div>
-                {/* {this.props.doctors === null ? 'still loading...' : this.showList(this.props.doctors)} */}
-                {this.props.doctors === null ? 'still loading...' : <DoctorList doctors={this.props.doctors}/>}
-            </div>
-        )
-    }
-}
+//     render() {
+//         return (
+//             <div>
+//                 {/* {this.props.doctors === null ? 'still loading...' : this.showList(this.props.doctors)} */}
+//                 {this.props.doctors === null ? 'still loading...' : <DoctorList doctors={this.props.doctors}/>}
+//             </div>
+//         )
+//     }
+// }
 
-const mapStateToProps = ({doctors}) => {
-    return {
-        doctors
-    }
-}
+// const mapStateToProps = ({doctors}) => {
+//     return {
+//         doctors
+//     }
+// }
 
-export default connect(mapStateToProps)(DoctorContainer); 
+// export default connect(mapStateToProps)(DoctorContainer); 
