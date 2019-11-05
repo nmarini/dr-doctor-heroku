b650a1e847a56b9c116c690e54a7b8a18dbca3d5
nmarini <nmarini44@gmail.com>
2019-11-04 18:16:35 -0800

    updated doctors to be from boston

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 user1 = User.create(name: "nick", email: 'nick.com', password: 'password')
-doctor1 = Doctor.create( last_name: "Anderson", uid: "039898ea958ba96aca4e2c28494d0b02", user_note: "let's get this fixed!", user_id: 1)
+doctor1 = Doctor.create( last_name: "Agus", uid: "1994f2d6de4b3ab4670273ecaa83f906", user_note: "let's get this fixed!", user_id: 1)
 
 user2 = User.create(name: "bob", email: 'bob.com', password: 'password')
-doctor2 = Doctor.create( last_name: "Leong", uid: "0ca043e9f1bab816d5fdbebf087c8f30", user_note: "let's get this fixed!", user_id: 2)
+doctor2 = Doctor.create( last_name: "Ahn", uid: "07cf5b43243aab82c00272bd174da1ad", user_note: "let's get this fixed!", user_id: 2)
