6caeedb7df919cf637dfa2b0b3ff6d9b1b6c9e58
nmarini <nmarini44@gmail.com>
2019-12-12 17:29:49 -0800

    added 'react-router-dom'

---
+++ public/precache-manifest.ef4119912b03e70ca43597823aa8a60d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3010f84dc4808cc681b51c89b593cd5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36c47651c5c5033960c1",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "a3d771583163a09d952e",
+    "url": "/static/css/main.3907e60c.chunk.css"
+  },
+  {
+    "revision": "36c47651c5c5033960c1",
+    "url": "/static/js/2.e9c72a1a.chunk.js"
+  },
+  {
+    "revision": "a3d771583163a09d952e",
+    "url": "/static/js/main.a57b490a.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  }
+]);
