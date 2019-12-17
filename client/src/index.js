c0deedaf73fec47ee5362a08a0431b80b5a01623
nmarini <nmarini44@gmail.com>
2019-12-16 18:00:12 -0800

    added homepage to package.json

--- client/src/index.js
+++ client/src/index.js
@@ -8,16 +8,3 @@ require('dotenv').config();
 
 ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("/*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
-  }
-  
-  else {
-    app.use(express.static(path.join(__dirname, '/client/public')));
-    app.get("/*", function(req, res) {
-      res.sendFile(path.join(__dirname, "./client/public/index.html"));
-    });
-  }
